87dffad5175b56dededa1407c12c691c00bc0c86
Victor Maeiro Mendes da Silva <victor.silva@payly.com.br>
2024-05-18 16:47:34 -0300

    db test

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3002;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
   console.log('Backend iniciado com sucesso!');
