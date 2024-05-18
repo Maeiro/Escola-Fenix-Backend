f96a6dff50e9bf9a6af8b0c73b8f75f8938f1810
Victor Maeiro Mendes da Silva <victor.silva@payly.com.br>
2024-05-18 16:05:45 -0300

    added heroku env to server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
+  console.log('Backend iniciado com sucesso!');
 });
