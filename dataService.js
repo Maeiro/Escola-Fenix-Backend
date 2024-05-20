4439aec6accf22ea5a440c5be79803e7d730f90c
Victor Maeiro Mendes da Silva <victor.silva@payly.com.br>
2024-05-20 16:46:05 -0300

    refactor

--- dataService.js
+++ dataService.js
@@ -69,8 +69,10 @@ async function buildFilteredQuery(baseQuery, filters) {
 
   for (const [key, value] of Object.entries(filters)) {
     if (value !== undefined && value !== '') {
-      const column = key === 'presente' ? 'presencas.presente' : key === 'total_faltas' ? 'alunos.total_faltas' : `alunos.${key}`;
-      const operator = key === 'presente' ? '=' : 'ILIKE';
+      const column = key === 'presente' ? 'presencas.presente' :
+        key === 'totalFaltas' ? 'alunos.total_faltas' :
+          key === 'data' ? 'presencas.data' : `alunos.${key}`;
+      const operator = key === 'presente' || key === 'data' ? '=' : 'ILIKE';
       queryParams.push(key === 'presente' ? value === 'true' : `%${value}%`);
       query += ` AND ${column} ${operator} $${queryParams.length}`;
     }
